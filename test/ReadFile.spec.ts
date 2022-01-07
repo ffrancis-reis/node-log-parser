c7258e937395cf98bd84e844b549dfe33c0274dc
Felipe Reis <ffrancis.reis@gmail.com>
2022-01-07 02:14:45 -0300

    Fix jest mock file

--- test/ReadFile.spec.ts
+++ test/ReadFile.spec.ts
@@ -7,12 +7,10 @@ describe('read file', () => {
   const mockFS: jest.Mocked<typeof fs> = <jest.Mocked<typeof fs>>fs
 
   it('should read a file', () => {
-    const file = 'app.log'
-
     mockFS.existsSync.mockReturnValue(true)
-    mockFS.readFileSync(file, 'utf8')
+    mockFS.readFileSync('file', 'utf8')
 
-    const readFile = new ReadFile(file)
+    const readFile = new ReadFile('file')
     readFile.executeAction()
 
     expect(mockFS.readFileSync).toHaveBeenCalled()
--- test/WriteFile.spec.ts
+++ test/WriteFile.spec.ts
@@ -1,9 +1,15 @@
 import { IError } from '../src/CreateErrors'
 import { WriteFile } from '../src/WriteFile'
+import fs from 'fs'
+
+jest.mock('fs')
 
 describe('write file', () => {
+  const mockFS: jest.Mocked<typeof fs> = <jest.Mocked<typeof fs>>fs
+
   it('should write a file', () => {
-    const file = 'app.log'
+    mockFS.writeFileSync('file', 'utf8')
+
     const error: IError = {
       err: 'critical error',
       loglevel: 'error',
@@ -11,17 +17,20 @@ describe('write file', () => {
       transactionId: '9abc55b2-807b-4361-9dbe-aa88b1b2e978'
     }
 
-    const writeFile = new WriteFile(error, file)
+    const writeFile = new WriteFile(error, 'file')
     writeFile.executeAction()
 
     expect(writeFile.data).toBeTruthy()
+    expect(mockFS.writeFileSync).toHaveBeenCalled()
   })
 
   it('should write a file w/o errors', () => {
-    const file = 'app.log'
-    const writeFile = new WriteFile({}, file)
+    mockFS.writeFileSync('file', 'utf8')
+
+    const writeFile = new WriteFile({}, 'file')
     writeFile.executeAction()
 
     expect(writeFile.data).toEqual({})
+    expect(mockFS.writeFileSync).toHaveBeenCalled()
   })
 })
